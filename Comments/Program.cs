e75e3e7a15a66420eba3265ba7c28a33d5cf1192
Anushree1811 <anushri1811@gmail.com>
2024-01-02 13:44:52 +0530

    Add project files.

---
+++ Comments/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AppraisalComment1 = null;
+            string AppraisalComment2 = "Test 1";
+            string AppraisalComment3 = null;
+
+            string M_RequestdotX26 =  !string.IsNullOrEmpty(AppraisalComment1)
+                                        ? AppraisalComment1
+                                        : (!string.IsNullOrEmpty(AppraisalComment2)
+                                            ? AppraisalComment2
+                                            : AppraisalComment3);
+
+
+            string M_RequestdotX27 = !string.IsNullOrEmpty(AppraisalComment1)
+                                    ? (!string.IsNullOrEmpty(AppraisalComment2)
+                                        ? AppraisalComment2
+                                        : AppraisalComment3)
+                                    : (!string.IsNullOrEmpty(AppraisalComment2)
+                                        ? AppraisalComment3
+                                        : string.Empty);
+
+            string M_RequestdotX28 = !string.IsNullOrEmpty(AppraisalComment1)
+                                    ? (!string.IsNullOrEmpty(AppraisalComment2)
+                                        ? AppraisalComment3
+                                        : string.Empty)
+                                    : string.Empty;
+
+            Console.WriteLine("M_RequestdotX26 " + M_RequestdotX26);
+            Console.WriteLine("M_RequestdotX27 " + M_RequestdotX27);
+            Console.WriteLine("M_RequestdotX28 " + M_RequestdotX28);
+        }
+    }
+}
